76f4edcf66ae7510a567ad68dc62c129a596eab5
devengup <devendragupta.1793@gmail.com>
2026-06-18 21:49:09 +0200

    changes helloWorld

--- MyProject2026/force-app/main/default/lwc/helloWorld/helloWorld.js
+++ MyProject2026/force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,17 +1,18 @@
 import { LightningElement } from 'lwc';
 import { track } from 'lwc';
 import { wire } from 'lwc';
+import { getRecord } from 'lightning/uiRecordApi';
 import createContact from '@salesforce/apex/ContactController.createContact';
 export default class HelloWorld extends LightningElement {
  greeting ='World !'
-@wire(getRecords , { recordId: '$recordId' ,fields: ['Name', 'Email']}) accountRecord;                 
+@wire(getRecord, { recordId: '$recordId' ,fields: ['Name', 'Email']}) accountRecord;                 
  changeHandler(event){
     this.greeting=event.target.value
  }
  createContactHandler(){
 
-    createContact({firstName:accountRecord.data.Name
-                    email:accountRecord.data.Email
+    createContact({firstName:this.accountRecord.data.Name,
+                    email:this.accountRecord.data.Email
     })
     .then(result=>{
         console.log('result===>',result)
